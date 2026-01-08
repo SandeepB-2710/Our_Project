fdf52bebd7b803e66b0a6e2605d43448a334f1a8
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-01-08 15:30:49 +0530

    Added Category API and ignored application.properties

---
+++ src/main/java/com/tata/repo/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.tata.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tata.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
---
+++ src/main/java/com/tata/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.tata.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tata.payloads.CategoryDto;
+
+@Service
+public interface CategoryService {
+
+	CategoryDto saveCategory(CategoryDto categoryDto);
+	
+	CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId);
+	
+	public String deleteCategeory(Integer categeoryId);
+	
+	CategoryDto getCategoryById(Integer categoryId);
+	
+	List<CategoryDto> getAllCategories();
+}
